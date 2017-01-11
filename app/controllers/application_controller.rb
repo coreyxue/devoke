eb533bd64769fd00209ac31797c3de726dbfebb7
Corey <xiaoxi.xue@adap.tv>
2017-01-11 01:10:19 -0800

    finishing up welcome page UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  #check if user account is newly created, if it is enforce the creation of user_information
   before_filter :user_has_information?, :except=>[:new, :create]
   def user_has_information?
   	if current_user and current_user.user_information.nil?
